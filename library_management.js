c47762b0316f2881ff8e22540e6a448fab95a0b7
Jorge Bernal <jorgebernal@usf.edu>
2024-10-20 15:01:55 -0400

    Create Book class with properties and methods

---
+++ library_management.js
@@ -0,0 +1,20 @@
+class Book {
+    constructor(title, author, ISBN){
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this._isAvailable = true;
+    };
+getDetails() {
+    return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`
+};
+
+get isAvailable(){
+    return this._isAvailable;
+};
+
+set isAvailable(status) {
+    this._isAvailable = status;
+};
+
+};
